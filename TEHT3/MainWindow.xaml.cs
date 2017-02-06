16b0fabb0aaec453525124a29cfb1474dc598a3b
lauri virtanen <lavir87@gmail.com>
2017-02-06 18:36:05 +0200

    -

--- TEHT3/MainWindow.xaml.cs
+++ TEHT3/MainWindow.xaml.cs
@@ -23,23 +23,52 @@ namespace TEHT3
 
     public partial class MainWindow : Window
     {
-        private int kpuu = 45;
+        private float leveysMm = 0f;
+        private float korkeusMm = 0f;
+        private float pinAla;
+        private float kpuu = 45;
+        private float karpir = 0f;
+        private float test;
+
         public MainWindow()
         {
+            
+            pintaAla();
+            KarmiPinnat();
             InitializeComponent();
+            karmil.Text = kpuu.ToString();
+        }
+
+        private float pintaAla()
+        {
+            pinAla = (leveysMm * korkeusMm);
+            return pinAla;
+        }
+
+        private float karmiPiiri()
+        {
+            karpir = (leveysMm * 2 + korkeusMm * 2)/10;
+            return karpir;
+        }
+
+        private float KarmiPinnat()
+        {
+            test = ((leveysMm * korkeusMm) / 10) + ((kpuu * korkeusMm) * 2 + (kpuu * leveysMm) * 2)/10 - (kpuu * 8)/10;
+
+            return test;
         }
 
         private void OnClick(object sender, RoutedEventArgs e)
         {
-            float leveys;
-            float korkeus;
-
-            float.TryParse(textBox1.Text, out korkeus);
-
-            if (float.TryParse(textBox.Text, out leveys))
+            if (float.TryParse(leveys.Text, out leveysMm) && float.TryParse(korkeus.Text, out korkeusMm) && float.TryParse(karmil.Text, out kpuu))
             {
-                
-                textBox4.Text = (leveys * korkeus).ToString("F00") + " cm^2";
+                pintaAla();
+                KarmiPinnat();
+                karmiPiiri();
+
+                IkkunaPa.Text = test.ToString("F00")+ " CM^2";
+                LasinPa.Text = ((leveysMm * korkeusMm) / 10).ToString("F00") + " CM^2"; 
+                KarmiPiiri.Text = karpir.ToString("F00")+" cm";
             }
         }
     }
