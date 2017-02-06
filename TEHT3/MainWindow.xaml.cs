d7225a50609343288d414df3d3a9825b3c1d53fa
K8455 <K8455@ICT-421-WS-38>
2017-02-06 13:44:15 +0200

    Add project files.

---
+++ TEHT3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TEHT3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        private int kpuu = 45;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            float leveys;
+            float korkeus;
+
+            float.TryParse(textBox1.Text, out korkeus);
+
+            if (float.TryParse(textBox.Text, out leveys))
+            {
+                
+                textBox4.Text = (leveys * korkeus).ToString("F00") + " cm^2";
+            }
+        }
+    }
+}
---
+++ TTO0300/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TTO0300
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            { 
+            int j;
+            
+            if (Int32.TryParse(textBlock.Text, out j)) { j++;  }
+            textBlock.Text = j.ToString();
+
+            }catch(Exception ex)
+            {
+
+               MessageBox.Show("Error",ex.Message);
+            }
+        }
+
+        private void OnClick2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int j;
+                if (Int32.TryParse(textCars.Text, out j)) { j++; }
+                textCars.Text = j.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error", ex.Message);
+            }
+            /* Toisen ikkunan avaamista
+            sup supp = new TTO0300.sup();
+            supp.ShowDialog();*/
+        }
+    }
+}
