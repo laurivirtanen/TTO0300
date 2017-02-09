6795de557c3288c5f401b3cbb8907212db1b0424
lauri virtanen <lavir87@gmail.com>
2017-02-10 00:14:48 +0200

    -

--- TEHT-3/Class.cs
+++ TEHT-3/Class.cs
@@ -8,35 +8,204 @@ namespace TEHT_3
 {
     class Lotto
     {
-        Random rand = new Random();
 
-        public string Lottery(string lot, int draws, int game)
+        Random rand = new Random();
+        private int randomNumber;
+        public int[,] allthemdraws;
+        public int game = 7;
+        private int maxNumber;
+
+        public string Eurojackpot(int draws, string lot)
         {
-            int[] t = new int[game];
-            int x;
+            rand = new Random();
+            game = 5;
+            maxNumber = 51;
+
+            int[,] allDraws = new int[draws, game];
+
+            int[] draw = new int[game];
             for (int i = 0; i < draws; i++)
             {
-                lot+="Row " +(i+1)+" : ";
+                lot += "Row " + (i + 1) + " : ";
                 for (int j = 0; j < game; j++)
                 {
                     do
                     {
-                        x = rand.Next(1, 40);
-                    } while (t.Contains(x));
-                    t[j] = x;
+                        randomNumber = rand.Next(1, maxNumber);
+                    } while (draw.Contains(randomNumber));
+                    allDraws[i, j] = randomNumber;
+                    draw[j] = randomNumber;
+                }
+                Array.Sort(draw);
+
+                for (int h = 0; h < draw.Length; h++)
+                {
+                    lot += draw[h].ToString();
+                    if (h < game - 1)
+                    {
+                        lot += ", ";
+                    }
+                }
+                int[] stars = new int[2];
+                stars[0] = rand.Next(1, 11);
+                do
+                {
+                    stars[1] = rand.Next(1, 11);
+                } while (stars[1] == stars[0]);
+                Array.Sort(stars);
+                lot += "*" + stars[0] + ", " + stars[1];
+                lot += "\n";
+
+            }
+
+            allthemdraws = allDraws;
+            return lot;
+        }
+
+        public string VikingLotto(int draws, string lot)
+        {
+
+            rand = new Random();
+            game = 6;
+            maxNumber = 40;
+            int[] draw = new int[game];
+            int[,] allDraws = new int[draws, game];
+            int randomNumber;
+            for (int i = 0; i < draws; i++)
+            {
+                lot += "Row " + (i + 1) + " : ";
+                for (int j = 0; j < game; j++)
+                {
+                    do
+                    {
+                      randomNumber = rand.Next(1, maxNumber);
+                    } while (draw.Contains(randomNumber));
+
+                    allDraws[i, j] = randomNumber;
+                    draw[j] = randomNumber;
                 }
 
-                Array.Sort(t);
+                Array.Sort(draw);
 
                 for (int h = 0; h < game; h++)
                 {
-                    lot += t[h].ToString();
-                    lot += ", ";
+                    lot += draw[h].ToString();
+                    if (h < game - 1)
+                    {
+                        lot += ", ";
+                    }
                 }
                 lot += "\n";
             }
+            allthemdraws = allDraws;
             return lot;
-
         }
+
+        public string NormalLotto(int draws, string lot)
+        {
+            rand = new Random();
+            game = 7;
+            maxNumber = 40;
+            int[] draw = new int[game];
+            int[,] allDraws = new int[draws, game];
+            for (int i = 0; i < draws; i++)
+            {
+
+
+                lot += "Row " + (i + 1) + " : ";
+                for (int j = 0; j < game; j++)
+                {
+                    do
+                    {
+                        randomNumber = rand.Next(1, maxNumber);
+                    } while (draw.Contains(randomNumber));
+
+                    allDraws[i, j] = randomNumber;
+                    draw[j] = randomNumber;
+                }
+
+                Array.Sort(draw);
+
+                for (int h = 0; h < game; h++)
+                {
+                    lot += draw[h].ToString();
+                    if (h < game - 1)
+                    {
+                        lot += ", ";
+                    }
+                }
+                lot += "\n";
+
+            }
+            allthemdraws = allDraws;
+            return lot;
+        }
+
+        public string Lottery(string lot, int draws, int game)
+        {
+            switch (game)
+            {
+                case 1:
+                    return NormalLotto(draws, lot);
+                case 2:
+                    return VikingLotto(draws, lot);
+                case 3:
+                    return Eurojackpot(draws, lot);
+                default:
+                    break;
+            }
+
+
+            return "ERROR";
+        }
+
+
+        public string CheckWins()
+        {
+            int tulos = 0;
+            int[] result = new int[game];
+            string resultPrint = "";
+            int z;
+
+            for (int i = 0; i < game; i++)
+            {
+                do
+                {
+                    z = rand.Next(1, 40);
+                } while (result.Contains(z));
+                result[i] = z;
+                
+            }
+
+            Array.Sort(result);
+            foreach (var item in result)
+            {
+                resultPrint += item.ToString() + " ";
+            }
+
+            for (int i = 0; i < allthemdraws.GetLength(0); i++)
+            {
+                int oikein = 0;
+                for (int j = 0; j < allthemdraws.GetLength(1); j++)
+                {
+                    if (result.Contains(allthemdraws[i, j]))
+                    {
+                        oikein++;
+                    }
+                }
+
+                if (oikein == 7) { tulos += 2400000; resultPrint += "\n" + oikein + " oikein, voitit 2 400 000€"; resultPrint += oikein + " oikein, voitit 2 400 000€"; }
+                else if (oikein == 6) { tulos += 1000; }   //scvResult.Content +="\n"+ oikein + " oikein, voitit 1000€"; MessageBox.Show(oikein + " oikein, voitit 1000€"); }
+                else if (oikein == 5) { tulos += 50; }   // }// scvResult.Content += "\n" + oikein + " oikein, voitit 50€"; MessageBox.Show(oikein + " oikein, voitit 50€"); }
+                else if (oikein == 4) { tulos += 10; }   // }scvResult.Content += "\n" + oikein + " oikein, voitit 10€"; MessageBox.Show(oikein + " oikein, voitit 10€"); }
+                else if (oikein == 3) { tulos += 1; }
+                {
+
+                }
+            }
+            resultPrint += "\nVoitit: " + tulos.ToString() + " euroa" +"\nKäytit: "+ allthemdraws.GetLength(0).ToString() +" euroa";
+            return resultPrint;
+        }
+      }
     }
-}
+
--- TEHT-3/MainWindow.xaml.cs
+++ TEHT-3/MainWindow.xaml.cs
@@ -21,58 +21,113 @@ namespace TEHT_3
     public partial class MainWindow : Window
     {
         string lottoPrint = "";
+        Image image = new Image();
+        ImageBrush myBrush = new ImageBrush();
+        Lotto lott = new Lotto();
 
         public MainWindow()
         {
             InitializeComponent();
-            
-        }
 
+        }
+        
         private void btnDraw_Click(object sender, RoutedEventArgs e)
         {
+            
 
-            ImageBrush myBrush = new ImageBrush();
-            Image image = new Image();
-            image.Source = new BitmapImage(new Uri("file:///D:/K8455/TTO0300/TEHT-3/Viking-Lotto.PNG"));
-            myBrush.ImageSource = image.Source;
-
-
-
-
-
-            Lotto lott = new Lotto();
-            int x = 2;
+            //Lotto lott = new Lotto();
+            int x = 0;
             Int32.TryParse(txtGames.Text.ToString(), out x);
-            if (lot.IsSelected==true)
+            if (lot.IsSelected == true)
             {
-                image.Source = new BitmapImage(new Uri("file:///D:/K8455/TTO0300/TEHT-3/lotto.PNG"));
-                myBrush.ImageSource = image.Source;
-                gridi.Background = myBrush;
-                txtPrint.Text = lott.Lottery(lottoPrint, x, 7);
+                scvResult.Content = lott.Lottery(lottoPrint, x, 1);
             }
             else if (vlot.IsSelected == true)
             {
-                gridi.Background = myBrush;
-                txtPrint.Text = lott.Lottery(lottoPrint, 55, 6);
+               
+                scvResult.Content = lott.Lottery(lottoPrint, x, 2);
             }
             else if (elot.IsSelected == true)
             {
-                image.Source = new BitmapImage(new Uri("file:///D:/K8455/TTO0300/TEHT-3/eurojackpot.PNG"));
-                myBrush.ImageSource = image.Source;
-                gridi.Background = myBrush;
-                txtPrint.Text = lott.Lottery(lottoPrint, x, 5);
+                scvResult.Content = lott.Lottery(lottoPrint, x, 3);
             }
 
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
-            txtPrint.Text = "";
+            scvResult.Content = "";
         }
 
         private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (lot.IsSelected == true)
+            {
+                image.Source = new BitmapImage(new Uri("http://lottonetti.net/wp-content/uploads/2016/04/veikkaus-lotto-logo.png"));
+                myBrush.ImageSource = image.Source;
+                gridi.Background = myBrush;
+            }
+            else if (vlot.IsSelected == true)
+            {
+                image.Source = new BitmapImage(new Uri("https://i1.wp.com/www.balkaneu.com/wp-content/uploads/2016/07/Euros.jpg?fit=565%2C424"));
+                myBrush.ImageSource = image.Source;
+                gridi.Background = myBrush;
+            }
+            else if (elot.IsSelected == true)
+            {
+                image.Source = new BitmapImage(new Uri("https://media.jumbolotto.de/00ca1e8/images/pub/meta/de/eurojackpot.jpg"));
+                myBrush.ImageSource = image.Source;
+                gridi.Background = myBrush;
+            }
         }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(lott.CheckWins());
+            /*
+            try
+            {
+                int tulos = 0;
+                Random test = new Random();
+                int[] result = new int[lott.game];
+                int z;
+
+                for (int i = 0; i < lott.game; i++)
+                {
+                    do
+                    {
+                        z = test.Next(1, 40);
+                    } while (result.Contains(z));
+                    result[i] = z;
+                }
+
+                    scvResult.Content = "";
+                for (int i = 0; i < lott.allthemdraws.GetLength(0); i++)
+                {
+                    int oikein = 0;
+                    for (int j = 0; j < lott.allthemdraws.GetLength(1); j++)
+                    {
+                        if (result.Contains(lott.allthemdraws[i, j]))
+                        {
+                            oikein++;
+                        }
+                    }
+
+                    if (oikein == 7) { tulos += 2400000; scvResult.Content += "\n" + oikein + " oikein, voitit 2 400 000€"; MessageBox.Show(oikein + " oikein, voitit 2 400 000€"); }
+                    else if (oikein == 6) { tulos += 1000; }   //scvResult.Content +="\n"+ oikein + " oikein, voitit 1000€"; MessageBox.Show(oikein + " oikein, voitit 1000€"); }
+                    else if (oikein == 5) { tulos += 50; }   // }// scvResult.Content += "\n" + oikein + " oikein, voitit 50€"; MessageBox.Show(oikein + " oikein, voitit 50€"); }
+                    else if (oikein == 4) { tulos += 10; }   // }scvResult.Content += "\n" + oikein + " oikein, voitit 10€"; MessageBox.Show(oikein + " oikein, voitit 10€"); }
+
+                    { 
+
+                }
+            }
+                MessageBox.Show("Voitit: " + tulos.ToString()) + " euroa";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }*/
+        }
+        
     }
 }
